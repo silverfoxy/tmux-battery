3b83a24babc73393d3d8812a95c4f062cb577f82
Babak AminAzad <baminazad@cs.stonybrook.edu>
2024-09-15 10:53:00 -0400

    Fix termux battery icons

--- scripts/battery_icon.sh
+++ scripts/battery_icon.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$CURRENT_DIR/helpers.sh"
 
 print_icon() {
-	local status=$1
-	if [ "$status" == "discharging" ]; then
-		$CURRENT_DIR/battery_icon_charge.sh
-	else
-		$CURRENT_DIR/battery_icon_status.sh "$status"
-	fi
+  local status=$1
+  if [ "$status" == "discharging" ]; then
+    $CURRENT_DIR/battery_icon_charge.sh
+  else
+    $CURRENT_DIR/battery_icon_status.sh "$status"
+  fi
 }
 
 main() {
-	local status=$(battery_status)
-	print_icon "$status"
+  local status=$(battery_status)
+  print_icon "$status"
 }
 
 main
